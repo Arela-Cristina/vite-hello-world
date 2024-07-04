161eabb4737cbe181c74bc7f1bf7a397b2198042
Arela-Cristina <instagrandine@gmail.com>
2024-07-04 18:27:16 +0200

    Bonus finish

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<!-- codice Javascript -->
+<script>
+export default {
+    name: "AppHero",
+}
+
+</script>
+
+<!-- Codice HTML -->
+<template>
+    <img src="/public/dance.gif">
+</template>
+
+<!-- Codice Css -->
+<style scoped>
+img {
+    width: 80%;
+}
+</style>
