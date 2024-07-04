fc8d3bb2cdcc0870a07f25d4f09a2298f44522a0
Arela-Cristina <instagrandine@gmail.com>
2024-07-04 17:35:10 +0200

    errore

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<!-- codice Javascript -->
+<script>
+export default {
+    name: "AppTitle",
+}
+
+</script>
+
+<!-- Codice HTML -->
+<template>
+    <h1>La mia Prima App con Vite ☺</h1>
+</template>
+
+<!-- Codice Css -->
+<style scoped></style>
